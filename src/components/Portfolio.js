1c3b24aa3e5d4b2139d61cac1bbf9035468296b1
Adesh Kumar <Adesh.kumar@bold.com>
2020-06-19 23:46:32 +0530

    added portfolio design

---
+++ src/components/Portfolio.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import project from '../assets/projects/1.jpg';
+import '../styles/portfolio.scss';
+function Portfolio() {
+  return (
+    <div className='portfolio'>
+      <div className='portfolio__title'>Portfolio</div>
+      <div className='project'>
+        <div className='project__list'>
+          <div className='project__fig'>
+            <img src={project} alt='project_theme' className='project__image' />
+          </div>
+          <div className='project__title'>iChamp India</div>
+          <a href='goo' target='_blank' className='project__url'>
+            <i className='fa fa-address-book'></i>
+          </a>
+        </div>
+        <div className='project__list'>
+          <div className='project__fig'>
+            <img src={project} alt='project_theme' className='project__image' />
+          </div>
+          <div className='project__title'>Livecareer</div>
+          <a href='goo' target='_blank' className='project__url'>
+            <i className='fa fa-address-book'></i>
+          </a>
+        </div>{' '}
+        <div className='project__list'>
+          <div className='project__fig'>
+            <img src={project} alt='project_theme' className='project__image' />
+          </div>
+          <div className='project__title'>Livecareer</div>
+          <a href='goo' target='_blank' className='project__url'>
+            <i className='fa fa-address-book'></i>
+          </a>
+        </div>{' '}
+        <div className='project__list'>
+          <div className='project__fig'>
+            <img src={project} alt='project_theme' className='project__image' />
+          </div>
+          <div className='project__title'>Livecareer</div>
+          <a href='goo' target='_blank' className='project__url'>
+            <i className='fa fa-address-book'></i>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Portfolio;
