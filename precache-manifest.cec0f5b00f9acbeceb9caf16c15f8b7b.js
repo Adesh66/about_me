4bb6187d60cf6fdd6f85a92968d33a0f03e7d8bc
Adesh Kumar <Adesh.kumar@bold.com>
2020-06-20 09:44:11 +0530

    Updates

---
+++ precache-manifest.cec0f5b00f9acbeceb9caf16c15f8b7b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "324ffd27b379cbc77d7a8ac715bc556c",
+    "url": "/about_me/index.html"
+  },
+  {
+    "revision": "754a398eb0e18c356b9a",
+    "url": "/about_me/static/css/main.32463614.chunk.css"
+  },
+  {
+    "revision": "2f7264a21f881378b315",
+    "url": "/about_me/static/js/2.9e8c5896.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/about_me/static/js/2.9e8c5896.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "754a398eb0e18c356b9a",
+    "url": "/about_me/static/js/main.d84d3678.chunk.js"
+  },
+  {
+    "revision": "79e03bcf4f0cae51ff99",
+    "url": "/about_me/static/js/runtime-main.88240aa2.js"
+  },
+  {
+    "revision": "82ece44f04e3639be2865d157d9c80b4",
+    "url": "/about_me/static/media/1.82ece44f.jpg"
+  },
+  {
+    "revision": "98980409f06c1c0b2d856c06b419b1be",
+    "url": "/about_me/static/media/admin.98980409.jpg"
+  }
+]);
