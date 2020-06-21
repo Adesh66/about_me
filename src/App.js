0a8541ee7d2f1e4dbfc0fe4b489492f9c9bf1c5d
Adesh Kumar <Adesh.kumar@bold.com>
2020-06-21 17:11:37 +0530

    resolved

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import Header from './components/Header';
-import Main from './components/Main';
-
+import Intro from './components/Intro';
+import Portfolio from './components/Portfolio';
+import Footer from './components/Footer';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Notfound from './components/Notfound';
+import Contact from './components/Contact';
 function App() {
   return (
     <div className='App'>
-      <Header />
-      <Main />
+      <Router>
+        <Header />
+        <Switch>
+          <Route path='/' exact component={Intro} />
+          <Route path='/portfolio' component={Portfolio} />
+          {/* <Route path='/about' component={About} /> */}
+          <Route path='/contact' component={Contact} />
+          <Route component={Notfound} />
+        </Switch>
+        <Footer />
+      </Router>
     </div>
   );
 }
---
+++ src/components/Intro.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import logo from '../assets/admin.jpg';
+import MyPDF from '../assets/cv.pdf';
+import { Link } from 'react-router-dom';
+
+function Intro() {
+  return (
+    <div className='banner'>
+      <div className='banner__fig'>
+        <img src={logo} alt='my_profile_picture' />
+      </div>
+      <div className='caption-box'>
+        <div className='caption-box__designation'>UI Engineer</div>
+        <div className='caption-box__title'>
+          <span className='caption-box__title__bold'>Adesh</span> Kumar
+        </div>
+        <div className='caption-box__description'>
+          I'm a UI Engineer with 3 years of experience in UI development and
+          currently working with BOLD INDIA. I'm quite familar with developing
+          the core products and handling the larger applications. When I am not
+          busy solving a technology problem, I love cooking and catching a
+          movie.
+        </div>
+        <div className='action-btn'>
+          <Link
+            to={MyPDF}
+            target='_blank'
+            className='action-btn__home action-btn__home__download'
+            download
+          >
+            Download CV
+          </Link>
+          <Link to="/contact" className='action-btn__home action-btn__home__contact'>
+            Contact
+          </Link>
+        </div>
+      </div>
+      <div className='text-bg'>
+        <span>Adesh</span>
+      </div>
+    </div>
+  );
+}
+
+export default Intro;
