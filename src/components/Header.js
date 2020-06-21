0586f12d701f16f2d3b05499f50473052bc9f30d
Adesh Kumar <Adesh.kumar@bold.com>
2020-06-21 23:07:24 +0530

    added media queries and hamburger for menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,20 +2,49 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Link } from 'react-router-dom';
 
 export class Header extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      menuactive: false,
+    };
+  }
+
+  toggleHandle = () => {
+    console.log(this.state.menuactive);
+    this.setState((prevstate) => ({
+      menuactive: !prevstate.menuactive,
+    }));
+  };
+
   render() {
     return (
-
       <header className='header'>
-        <Link to="/" className='header-logo'>
-          <span className='header-logo__break'>A</span>desh
-        </Link>
-        <div className='header-theme'>
-          <span className='header-theme__options header-theme__option--active'>
-            light
-          </span>
-          <span className='header-theme__options'>Dark</span>
+        <div className='navigation-menu'>
+          <Link to='/' className='header-logo'>
+            <span className='header-logo__break'>A</span>desh
+          </Link>
+          <div className='header-theme'>
+            <span className='header-theme__options header-theme__option--active'>
+              light
+            </span>
+            <span className='header-theme__options'>Dark</span>
+          </div>
+          <div
+            className={`hamburger ${this.state.menuactive ? `active` : ` `}`}
+            onClick={() => this.toggleHandle()}
+          >
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
         </div>
-        <ul className='menu-list'>
+
+        <ul
+          className={`menu-list ${
+            this.state.menuactive ? `menu-active` : `menu-hide`
+          }`}
+        >
           <li className='menu-list__item'>
             <Link className='menu-list__item__links' to='/'>
               About Me
@@ -31,11 +60,6 @@ export class Header extends Component {
               Portfolio
             </Link>
           </li>
-          {/* <li className='menu-list__item'>
-              <Link className='menu-list__item__links' to='/blog'>
-                Blog
-              </Link>
-            </li> */}
           <li className='menu-list__item menu-list__item--button'>
             <Link className='menu-list__item__links' to='/contact'>
               Contact
@@ -43,7 +67,6 @@ export class Header extends Component {
           </li>
         </ul>
       </header>
-
     );
   }
 }
