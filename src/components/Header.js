c27ff5a322f64fe46fef18c75e5629239dbdebd9
Adesh <Adeshgangwar1996@gmail.com>
2020-12-20 01:18:27 +0530

    theme changes added | home page changes | header and responsive issue in mobile

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Link } from 'react-router-dom';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Link } from "react-router-dom";
 
 export class Header extends Component {
   constructor(props) {
@@ -19,53 +19,52 @@ export class Header extends Component {
 
   render() {
     return (
-      <header className='header'>
-        <div className='navigation-menu'>
-          <Link to='/' className='header-logo'>
-            <span className='header-logo__break'>A</span>desh
-          </Link>
-          <div className='header-theme'>
-            <span className='header-theme__options header-theme__option--active'>
-              light
-            </span>
-            <span className='header-theme__options'>Dark</span>
+      <header className="header">
+        <nav class="navbar">
+          <div class="container">
+            <div className="inner-header">
+              <div class="navbar-header">
+                <button
+                  type="button"
+                  class="navbar-toggle"
+                  data-toggle="collapse"
+                  data-target="#myNavbar"
+                >
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+                </button>
+                <Link to="/" className="navbar-brand">
+                  <span className="navbar-brand__break">A</span>desh
+                </Link>
+              </div>
+              <div class="collapse navbar-collapse" id="myNavbar">
+                <ul class="nav navbar-nav navbar-right menu-list">
+                  <li className="menu-list__item">
+                    <Link className="menu-list__item__links" to="/">
+                      About Me
+                    </Link>
+                  </li>
+                  <li className="menu-list__item">
+                    <Link className="menu-list__item__links" to="/resume">
+                      Resume
+                    </Link>
+                  </li>
+                  <li className="menu-list__item">
+                    <Link className="menu-list__item__links" to="/portfolio">
+                      Portfolio
+                    </Link>
+                  </li>
+                  <li className="menu-list__item menu-list__item--button">
+                    <Link className="menu-list__item__links" to="/contact">
+                      Contact
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+            </div>
           </div>
-          <div
-            className={`hamburger ${this.state.menuactive ? `active` : ` `}`}
-            onClick={() => this.toggleHandle()}
-          >
-            <span></span>
-            <span></span>
-            <span></span>
-          </div>
-        </div>
-
-        <ul
-          className={`menu-list ${
-            this.state.menuactive ? `menu-active` : `menu-hide`
-          }`}
-        >
-          <li className='menu-list__item'>
-            <Link className='menu-list__item__links' to='/'>
-              About Me
-            </Link>
-          </li>
-          <li className='menu-list__item'>
-            <Link className='menu-list__item__links' to='/resume'>
-              Resume
-            </Link>
-          </li>
-          <li className='menu-list__item'>
-            <Link className='menu-list__item__links' to='/portfolio'>
-              Portfolio
-            </Link>
-          </li>
-          <li className='menu-list__item menu-list__item--button'>
-            <Link className='menu-list__item__links' to='/contact'>
-              Contact
-            </Link>
-          </li>
-        </ul>
+        </nav>
       </header>
     );
   }
--- src/components/Intro.js
+++ src/components/Intro.js
@@ -6,42 +6,49 @@ import { Link } from "react-router-dom";
 
 function Intro() {
   return (
-    <div className="banner">
-      <div className="alert-warn">In development mode.</div>
-      <div className="banner__fig">
-        <img src={logo} alt="my_profile_picture" />
-      </div>
-      <div className="caption-box">
-        <div className="caption-box__designation">UI Engineer</div>
-        <div className="caption-box__title">
-          <span className="caption-box__title__bold">Adesh</span> Kumar
+    <div className="main">
+      <div className="container">
+        <div className="banner">
+          <div className="alert-warn">In development mode.</div>
+          <div className="banner__fig">
+            <img src={logo} alt="my_profile_picture" />
+          </div>
+          <div className="caption-box">
+            <div className="caption-box__designation">UI Engineer</div>
+            <div className="caption-box__title">
+              <span className="caption-box__title__bold">Adesh</span> Kumar
+            </div>
+            <div className="caption-box__description">
+              I'm a UI Engineer with 3.6 years of experience in UI development
+              and currently working with{" "}
+              <a href="https://www.bold.com/" target="_blank" style={{color: '#f8991d'}}>
+                BOLD
+              </a>
+              . I'm quite familar with developing the core products and handling
+              the larger applications. When I am not busy solving a technical
+              problem, I love cooking and catching a movie.
+            </div>
+            <div className="action-btn">
+              <Link
+                to={MyPDF}
+                target="_blank"
+                className="action-btn__home action-btn__home__download"
+                download
+              >
+                Download CV
+              </Link>
+              <Link
+                to="/contact"
+                className="action-btn__home action-btn__home__contact"
+              >
+                Contact
+              </Link>
+            </div>
+          </div>
+          <div className="text-bg">
+            <span>Adesh</span>
+          </div>
         </div>
-        <div className="caption-box__description">
-          I'm a UI Engineer with 3 years of experience in UI development and
-          currently working with BOLD INDIA. I'm quite familar with developing
-          the core products and handling the larger applications. When I am not
-          busy solving a technology problem, I love cooking and catching a
-          movie.
-        </div>
-        <div className="action-btn">
-          <Link
-            to={MyPDF}
-            target="_blank"
-            className="action-btn__home action-btn__home__download"
-            download
-          >
-            Download CV
-          </Link>
-          <Link
-            to="/contact"
-            className="action-btn__home action-btn__home__contact"
-          >
-            Contact
-          </Link>
-        </div>
-      </div>
-      <div className="text-bg">
-        <span>Adesh</span>
       </div>
     </div>
   );
