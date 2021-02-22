07849e9c820dd15eb2d800927e6c6b8bd6b5eeb7
Adesh <Adeshgangwar1996@gmail.com>
2021-02-22 17:09:49 +0530

    a test case for volume up down in mobile

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -3,8 +3,26 @@ import React from "react";
 import logo from "../assets/admin.jpg";
 import MyPDF from "../assets/Adesh_CV.PDF";
 import { Link } from "react-router-dom";
+import { useEffect } from "react";
 
 function Intro() {
+  let keyDownHandler = (evt) => {
+    if (evt.key === "VolumeDown") {
+      alert("VolumeDown");
+    } else if (evt.key === "VolumeUp") {
+      alert("VolumeUp");
+    }
+    evt.preventDefault(); // to stop system app from processing keydown event
+  };
+
+  useEffect(() => {
+    if (window) {
+      window.addEventListener("keydown", keyDownHandler);
+    }
+    return () => {
+      window.removeEventListener("keydown", keyDownHandler);
+    };
+  }, []);
   return (
     <div className="main">
       <div className="container">
@@ -14,14 +32,20 @@ function Intro() {
             <img src={logo} alt="my_profile_picture" />
           </div>
           <div className="caption-box">
-            <h1 className="caption-box__designation" style={{margin: 0}}>UI Engineer</h1>
+            <h1 className="caption-box__designation" style={{ margin: 0 }}>
+              UI Engineer
+            </h1>
             <div className="caption-box__title">
               <span className="caption-box__title__bold">Adesh</span> Kumar
             </div>
             <div className="caption-box__description">
-              Hi, I'm a UI Engineer with 3.8+ years of experience in UI development
-              and currently working with{" "}
-              <a href="https://www.bold.com/" target="_blank" style={{color: '#f8991d'}}>
+              Hi, I'm a UI Engineer with 3.8+ years of experience in UI
+              development and currently working with{" "}
+              <a
+                href="https://www.bold.com/"
+                target="_blank"
+                style={{ color: "#f8991d" }}
+              >
                 BOLD
               </a>
               . I'm quite familar with developing the core products and handling
